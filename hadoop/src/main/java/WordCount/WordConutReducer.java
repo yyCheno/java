cd71dafbef969ea23dff7a22f0b3953a02373eb1
ccyy <15016250640@163.com>
2021-03-02 21:28:19 +0800

    增加八股文

---
+++ hadoop/src/main/java/WordCount/WordConutReducer.java
@@ -0,0 +1,4 @@
+package WordCount;
+
+public class WordConutReducer {
+}
---
+++ hadoop/src/main/java/WordCount/WordCountMapper.java
@@ -0,0 +1,15 @@
+package WordCount;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+public class WordCountMapper extends Mapper {
+
+    @Override
+    protected void map(Object key, Object value, Context context) throws IOException, InterruptedException {
+
+
+    }
+}
